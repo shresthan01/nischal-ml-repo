cfc1f0f442ebd3717a6f52bc2e03706f45f902af
Nischal Shrestha <sh.nischal90@gmail.com>
2023-01-07 13:35:38 -0800

    fast apiweek3

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -1,3 +1,38 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9e6c8d1c62d70814d14d1a19ccafec7769e146599a5c0d891bf5e03156aee858
-size 1004
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+from typing import List
+
+pipeline = pipeline("translation_en_to_ro", model='model/t5-small')
+
+app = FastAPI()
+
+class TextToTranslate(BaseModel):
+    input_text: str
+
+class TextToTranslateList(BaseModel):
+    input_text: List[str]
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+    
+@app.post("/echo")
+def echo(text_to_translate: TextToTranslate):
+    return {"message": text_to_translate.input_text}
+
+@app.post("/translate_text")
+def translate_text(text_to_translate: TextToTranslate):
+    return {"message": pipeline(text_to_translate.input_text)}
+
+@app.post("/translate_list_text")
+def translate_text(text_to_translate_list: TextToTranslateList):
+    return {"message": pipeline(text_to_translate_list.input_text)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
